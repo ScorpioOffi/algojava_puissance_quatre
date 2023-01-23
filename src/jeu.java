1562d5ea09359d901b8266b0660b78d3f5543a0a
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-23 16:20:41 +0100

    fix

--- src/jeu.java
+++ src/jeu.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class jeu {
+public class Jeu {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
--- src/menu.java
+++ src/menu.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class menu {
+public class Menu {
     
     private static void afficherUnMenu() {
         ArrayList<String> menu = new ArrayList<>();
