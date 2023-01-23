211ab6f347202345f122483b9aa3477422e91e25
leo <leoyng95@gmail.com>
2023-01-23 18:59:10 +0100

    committ

--- src/joueur.java
+++ src/joueur.java
@@ -1,3 +1,38 @@
 public class joueur {
+
+    private String name;
+    private char symbol;
+    private String color;
+
+}
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public char getsymbol() {
+        return symbol;
+    }
+
+    public void setsymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+
+    public void setcolor(String setcolor){
+        this.color = color;
+
+
+
     
 }
+
+
+
