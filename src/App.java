1a38235fd0894aec4e207279c6821f0ac8d97fc9
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-23 15:24:03 +0100

    start

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
-ù§l:kb ,jl ù:
-    $    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
 }
