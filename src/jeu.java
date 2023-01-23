07c31b87f1082f6bc3c61741e245b55cadd43f1a
leo <leoyng95@gmail.com>
2023-01-23 15:23:22 +0100

    first commit

---
+++ src/jeu.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class jeu {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+
+}
---
+++ src/menu.java
@@ -0,0 +1,7 @@
+public class menu {
+    
+    private static void afficherUnMenu() {
+        ArrayList<String> menu = new ArrayList<>();
+    }
+    
+}
