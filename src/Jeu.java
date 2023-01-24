f56df27e83dbfc181dbed984d12cb1bb338357d6
leo <leoyng95@gmail.com>
2023-01-24 10:37:19 +0100

    commit

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,7 +1,9 @@
+import model.Menu;
 
 public class Jeu {
 
     public static void main(String[] args) throws Exception {
         System.out.println("coucou");
+        Menu.afficher_menu_choice();
     }   
 }
