63b1ffe0e0ec58c32f05de30ac1900b7f394f74b
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-24 10:23:26 +0100

    save

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -14,56 +14,31 @@ public class Menu {
             String choix = _scan.nextLine();
             switch (choix) {
                 case "1":
-                    // Choisir 1 contre 1
-                    afficher_menu_secondaire();
+                    // Création du joueurs (nom, symbol, couleur)
+                    Joueur.creerJoueur();
+                    break;
+                case "2":
+                    // Choisir 1 contre l'IA
+                    select_level_IA();
                     String choix2= _scan.nextLine();
                     switch(choix2) {
                         case "1":
                             // Choisir le nom
-                            insertion_nom();
+                            IA.level_1();
                             break;
                         case "2":
                             // Sélection du symbole
-                            select_symbol();
+                            IA.level_2();
                             break;
                         case "3":
                             // Sélection de la couleur
-                            select_color();
-                            break;
-                        case "4":
-                            // Lancer la parrtie
-                            start();
-                            break;
-                        case "m":
-                            break;
-                        default:
-                            // Si aucune commande n'est reconnue
-                            System.out.println("Entrez une commande valide");
-                            break;
-                    }
-                    break;
-                case "2":
-                    // Choisir 1 contre l'IA
-                    afficher_menu_secondaire();
-                    String choix3= _scan.nextLine();
-                    switch(choix3) {
-                        case "1":
-                            // Choisir le nom
-                            insertion_nom();
-                            break;
-                        case "2":
-                            // Sélection du symbole
-                            select_symbol();
-                            break;
-                        case "3":
-                            // Sélection de la couleur
-                            select_color();
+                            IA.level_3();
                             break;
                         case "4":
                             // choisir la difficulté de l'IA
-                            select_level_IA();
+                            IA.level_4();
                             break;
-                        case "m":
+                        case "r":
                             break;
                         default:
                             // Si aucune commande n'est reconnue
@@ -71,7 +46,7 @@ public class Menu {
                             break;
                     }
                     break;
-                case "m":
+                case "q":
                     return;
                 default:
                     System.out.println("Entrez une commande valide");
@@ -91,44 +66,16 @@ public class Menu {
         }
     }
 
-    public static void afficher_menu_secondaire() {
-        ArrayList<String> menus = new ArrayList<String>();
-        menus.add("----- PUISSANCE 4 - MENU Secondaire ----");
-        menus.add("1- Choisir le nom");
-        menus.add("2- Sélectionner le symbole");
-        menus.add("3- Sélectionner la couleur");
-        menus.add("4- Lancer la partie");
-        menus.add("m- retour");
-        for (String menu : menus) {
-            System.out.println(menu);
-        }
-    }
-
-    public static void insertion_nom() {
-
-    }
-
-    public static void select_symbol() {
-
-    }
-
-    public static void select_color() {
-
-    }
-
     public static void select_level_IA() {
         ArrayList<String> menus = new ArrayList<String>();
         menus.add("----- PUISSANCE 4 - IA Difficulty ----");
         menus.add("1- Niveau 1");
         menus.add("2- Niveau 2");
         menus.add("3- Niveau 3");
-        menus.add("m- retour");
+        menus.add("4- Niveau 4");
+        menus.add("r- retour");
         for (String menu : menus) {
             System.out.println(menu);
         }
-    }
-
-    public static void start() {
-        
     }
 }
--- src/model/Pion.java
+++ src/model/Pion.java
@@ -1,5 +1,5 @@
 package model;
 
-public class IA {
+public class Pion {
     
 }
