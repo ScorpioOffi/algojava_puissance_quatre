eda567fbdc51d5bfa35184e9e67ee123385d95ec
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-24 12:28:24 +0100

    save

--- src/Jeu.java
+++ src/Jeu.java
@@ -3,8 +3,7 @@ import model.Menu;
 public class Jeu {
 
     public static void main(String[] args) throws Exception {
-        Menu.afficher_menus();
-    }   
+        Menu.afficher_menus();	
+	}
 
-    
 }
--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -32,15 +32,22 @@ public class Joueur { // on commence par créer une classe joueur
 
         Scanner scann = new Scanner(System.in); // on crée un scanner qui va nous permettre de récupérer les données entrées par l'utilisateur
         
-        System.out.println("Entre un pseudo mais soit original !!"); // on demande à l'utilisateur de rentrer son nom
+        System.out.println("Quel est votre nom ?"); // on demande à l'utilisateur de rentrer son nom
         String nom = scann.nextLine(); // on récupère le nom entré par l'utilisateur
 
         // choisir  ton pseudo
-        System.out.println("A toi de choisir ton symboles "); // on demande à l'utilisateur de rentrer son symbole
+        System.out.println("Quel symbol souhaitez vous ?"); // on demande à l'utilisateur de rentrer son symbole
         String symbols = scann.nextLine(); // on récupère le symbole entré par l'utilisateur
 
         // choisir  ton sym
-        System.out.println(" coloré ou pas??"); // on demande à l'utilisateur de rentrer sa couleur
+        System.out.println("Voici la liste des couleurs disponibles :"); 
+        System.out.println("- Jaune"); 
+        System.out.println("- Rouge"); 
+        System.out.println("- Bleu"); 
+        System.out.println("- Violet"); 
+        System.out.println("- Orange"); 
+        System.out.println(" ");
+        System.out.println("Quel couleur souhaitez vous ?"); // on demande à l'utilisateur de rentrer sa couleur
         String couleur = scann.nextLine(); // on récupère la couleur entrée par l'utilisateur
 
        // choisir  ta couleurss
