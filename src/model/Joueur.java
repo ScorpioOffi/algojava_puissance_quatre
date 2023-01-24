526111eb63331179da80df771bc1f6a779e9e51a
ScorpioOffi <117345081+ScorpioOffi@users.noreply.github.com>
2023-01-24 10:41:36 +0100

    push

--- src/model/Joueur.java
+++ src/model/Joueur.java
@@ -6,7 +6,7 @@ public class Joueur { // on commence par créer une classe joueur
     
     //on declare les variables de la classe joueur
     private String nom; // une variable nom de type String qui va nous permettre de stocker le nom du joueur
-    private String symbols; // le meme principe pour la variable symbols qui va nous permettre de stocker le symbole du joueur
+    private String symbol; // le meme principe pour la variable symbols qui va nous permettre de stocker le symbole du joueur
     private String couleur; // et la variable couleur qui va nous permettre de stocker la couleur du joueur
 
     // ON CRÉER LES METHODES DE LA CLASSE JOUEUR
@@ -15,7 +15,7 @@ public class Joueur { // on commence par créer une classe joueur
     }
 
     public String getSymb() { // on crée une méthode getSymb qui va nous permettre de récupérer la valeur de la variable symbols
-        return symbols;
+        return symbol;
     }
 
     public String getCouleur() { // on fait la même pour la variable couleur
@@ -24,7 +24,7 @@ public class Joueur { // on commence par créer une classe joueur
 
     public Joueur(String nom, String symbols, String couleur) { // on crée une méthode constructeur qui va nous permettre de créer un joueur
         this.nom = nom; // on initialise la variable nom
-        this.symbols = symbols; // on initialise la variable symbols
+        this.symbol = symbols; // on initialise la variable symbols
         this.couleur = couleur; // on initialise la variable couleur
     }
     
