145f3e840bf7f2b184594a87e9f658455b10485d
matlam27 <117271454+matlam27@users.noreply.github.com>
2023-01-25 14:48:19 +0100

    Update Jeu.java

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,9 +1,31 @@
 import java.util.Scanner;
 
+import model.Joueur;
 import model.Menu;
 import model.State_Game;
 
 public class Jeu {
+    public static void victoire(){
+        System.out.println("Victoire!");
+    }
+    public static void egalite(){
+        System.out.println("Egalité!");
+    }
+    public static void afficher_victoire_pion(int C, int L, char[][] tab){
+        for (int y = 0; y < L; y++) {
+            System.out.print(' ');
+            for (int x = 0; x < C; x++) {
+                System.out.print(" " + tab[x][y] + " ");
+            }
+            System.out.print(' ');
+            System.out.println();
+        }
+    }
+    public static void compare(int somme, int max){
+        if (somme > max){
+            max = somme;
+        } 
+    }
 
     public static void main(String[] args) throws Exception {
 
@@ -13,33 +35,33 @@ public class Jeu {
             Menu.afficher_menus();
         }
 
+        State_Game.setGameState(State_Game.PLAYING);
         if (State_Game.isGameState(State_Game.PLAYING)) {
-            Scanner scanner = new Scanner(System.in);
-            int N = 4;
+            Scanner _scan = new Scanner(System.in);
             int C = 7;
             int L = 6;
-            char[][] plateau = new char[C][L];
+            char[][] tab = new char[C][L];
             for (int x = 0; x < C; x++) {
                 for (int y = 0; y < L; y++) {
-                    plateau[x][y] = '.';
+                    tab[x][y] = '.';
                 }
             }
-            int gagnant = 0;
+            int winner = 0;
             for (int i = 1; i <= C * L; i++) {
 
-                for (int loop = 0; loop < C + 2 + 2 * C; loop++) System.out.print(' ');
+                for (int a = 0; a < C + 2 + 2 * C; a++) System.out.print(' ');
                 System.out.println();
 
                 for (int y = 0; y < L; y++) {
                     System.out.print(' ');
                     for (int x = 0; x < C; x++) {
-                        System.out.print(" " + plateau[x][y] + " ");
+                        System.out.print(" " + tab[x][y] + " ");
                     }
                     System.out.print(' ');
                     System.out.println();
                 }
 
-                for (int loop = 0; loop < C + 2 + 2 * C; loop++) System.out.print(' ');
+                for (int b = 0; b < C + 2 + 2 * C; b++) System.out.print(' ');
                 System.out.println();
 
                 System.out.println(" Au tour du joueur " + (i % 2 == 1 ? 'X' : 'O'));
@@ -47,30 +69,30 @@ public class Jeu {
                 int colonne = -1;
                 while (!placement) {
                     colonne = -1;
-                    String ligne = scanner.nextLine();
+                    String ligne = _scan.nextLine();
                     try {
                         colonne = Integer.valueOf(ligne);
 
                         if (colonne >= 1 && colonne <= C) {
-                            if (plateau[colonne - 1][0] != '.') {
-                                System.out.println("Colonne pleine, réitérez");
+                            if (tab[colonne - 1][0] != '.') {
+                                System.out.println("Colonne pleine, réessayez");
                             } else {
                                 placement = true;
                             }
                         } else {
-                            System.out.println("Nombre incorrect, réitérez");
+                            System.out.println("Nombre incorrect, réessayez");
                         }
 
                     } catch (Exception e) {
-                        System.out.println("Nombre incorrect, réitérez");
+                        System.out.println("Nombre incorrect, réessayez");
                     }
 
                 }
                 int rang = L - 1;
-                while (plateau[colonne - 1][rang] != '.') {
+                while (tab[colonne - 1][rang] != '.') {
                     rang--;
                 }
-                plateau[colonne - 1][rang] = (i % 2 == 1 ? 'X' : 'O');
+                tab[colonne - 1][rang] = (i % 2 == 1 ? 'X' : 'O');
                 char symbole = (i % 2 == 1 ? 'X' : 'O');
                 int max = 0;
                 int x;
@@ -80,91 +102,78 @@ public class Jeu {
                 x = colonne - 1;
                 y = rang;
                 somme = -1;
-                while (y >= 0 && x >= 0 && plateau[x][y] == symbole) {
+                while (y >= 0 && x >= 0 && tab[x][y] == symbole) {
                     y--;
                     x--;
                     somme++;
                 }
                 x = colonne - 1;
                 y = rang;
-                while (y < L && x < C && plateau[x][y] == symbole) {
+                while (y < L && x < C && tab[x][y] == symbole) {
                     y++;
                     x++;
                     somme++;
                 }
-                if (somme > max) max = somme;
-
+                compare(somme, max);
                 x = colonne - 1;
                 y = rang;
                 somme = -1;
-                while (y >= 0 && x < C && plateau[x][y] == symbole) {
+                while (y >= 0 && x < C && tab[x][y] == symbole) {
                     y--;
                     x++;
                     somme++;
                 }
                 x = colonne - 1;
                 y = rang;
-                while (y < L && x >= 0 && plateau[x][y] == symbole) {
+                while (y < L && x >= 0 && tab[x][y] == symbole) {
                     y++;
                     x--;
                     somme++;
                 }
-                if (somme > max) max = somme;
-
+                compare(somme, max);
                 x = colonne - 1;
                 y = rang;
                 somme = -1;
-                while (y >= 0 && plateau[x][y] == symbole) {
+                while (y >= 0 && tab[x][y] == symbole) {
                     y--;
                     somme++;
                 }
                 y = rang;
-                while (y < L && plateau[x][y] == symbole) {
+                while (y < L && tab[x][y] == symbole) {
                     y++;
                     somme++;
                 }
-                if (somme > max) max = somme;
-
+                compare(somme, max);
                 x = colonne - 1;
                 y = rang;
                 somme = -1;
-                while (x >= 0 && plateau[x][y] == symbole) {
+                while (x >= 0 && tab[x][y] == symbole) {
                     x--;
                     somme++;
                 }
                 x = colonne - 1;
-                while (x < C && plateau[x][y] == symbole) {
+                while (x < C && tab[x][y] == symbole) {
                     x++;
                     somme++;
                 }
-                if (somme > max) max = somme;
-
-
-                if (max >= N) {
-                    gagnant = (i % 2 == 1 ? 1 : 2);
+                compare(somme, max);
+                if (max >= 4) {
+                    winner = (i % 2 == 1 ? 1 : 2);
                     i = C * L + 1;
                 }
-
             }
-
-            for (int y = 0; y < L; y++) {
-                System.out.print(' ');
-                for (int x = 0; x < C; x++) {
-                    System.out.print(" " + plateau[x][y] + " ");
-                }
-                System.out.print(' ');
-                System.out.println();
+            if (winner == 0) {
+                afficher_victoire_pion(C, L, tab);
+                egalite();
             }
-
-            if (gagnant == 0) {
-                System.out.println("Match Nul");
+            if (winner== 1) {
+                afficher_victoire_pion(C, L, tab);
+                victoire();
             }
-
-
-            if (gagnant == 1) {
-                System.out.println("Victoire");
+            if (winner== 2) {
+                afficher_victoire_pion(C, L, tab);
+                victoire();
             }
-
         }
     }
 }
