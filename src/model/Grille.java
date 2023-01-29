893fd83b3b9fdcb99ce90c48c28bc017b03edb83
Adilina04 <113524418+Adilina04@users.noreply.github.com>
2023-01-29 15:32:48 +0100

    push pr pull

--- src/model/Grille.java
+++ src/model/Grille.java
@@ -1,5 +1,7 @@
 package model;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import model.Pion;
 
 public class Grille {
@@ -38,4 +40,39 @@ public class Grille {
     public void setGrid(ArrayList<ArrayList<String>> grid) {
         this.grille = grille;
     }
+
+
+
+    protected Joueur joueur;
+    protected String symbole;
+    protected String couleur;
+    
+    private static Scanner _scan = new Scanner(System.in);
+    private static final int ligne = 6;
+    private static final int colonne = 7;
+
+    int column = _scan.nextInt();
+    private ArrayList<ArrayList<Pion>> grille;
+
+    public void ajouterPion(Pion string, int column) {
+        if (column >= 0 && column <= colonne - 1 && !isColonnePleine(column)) {
+            for (int i = ligne - 1; i >= 0; i--) {
+                if (grille.get(i).get(column).equals(".")) {
+                    grille.get(i).set(column, string);
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Mauvaise colonne, réessayez");
+        }
+    }
+    
+    public boolean isColonnePleine(int column) {
+        if (!grille.get(0).get(column).equals(".")) {
+            return true;
+        }
+        return false;
+    }
+    
 }
+
--- src/model/IA.java
+++ src/model/IA.java
@@ -3,21 +3,19 @@ import model.Grille;
 public class IA {
 
     public static void level_1() {
-        Grille grille  = new Grille();
+
+
     }
 
   
 
     public static void level_2() {
-        Grille grille  = new Grille();
     }
 
     public static void level_3() {
-        Grille grille  = new Grille();
     }
 
     public static void level_4() {
-        Grille grille  = new Grille();
     }
     
 }
